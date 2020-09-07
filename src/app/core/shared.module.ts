6f546fa462e66f49df90edcafa710f727d521ede
rahul.dudhane <rahul.dudhane@mindbowser.com>
2020-09-07 12:21:04 +0530

    "Added examples"

--- src/app/core/shared.module.ts
+++ src/app/core/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TimeConvertPipe } from './pipes/time-converter/time-convert.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageNotFoundComponent, TimeConvertPipe],
   imports: [
     CommonModule
   ]
